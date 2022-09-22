f9d915ffb2c7f6a4003fd98ec34eca3665857096
Straton11 <stratonfacer1@gmail.com>
2022-09-22 09:13:52 -0600

    design finish

---
+++ scripts/dates.js
@@ -0,0 +1,4 @@
+// let text = document.lastModified;
+// document.getElementById("date").innerHTML = text;
+let date_changed = document.getElementById("date")
+date_changed.textContent = document.lastModified;
