65b8662e7642dd1fe476c351823f4edfaa2f214f
Straton11 <stratonfacer1@gmail.com>
2022-09-29 16:46:41 -0600

    changes

--- scripts/dates.js
+++
@@ -1,4 +0,0 @@
-// let text = document.lastModified;
-// document.getElementById("date").innerHTML = text;
-let date_changed = document.getElementById("date")
-date_changed.textContent = document.lastModified;
