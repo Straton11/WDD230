f9cd9ba6c5fa7b174846d23dbd98d0b5c7229f4e
Straton11 <stratonfacer1@gmail.com>
2022-11-22 14:01:05 -0700

    chamber

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -50,4 +50,13 @@ const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
 
+/*code for getting day of week written out*/
+let weekday = new Date().toLocaleString('en-us', {weekday:'long'});
+console.log(weekday);
+
+//code to toggle invite banner based on day of week being Tuesday or Wednesday
+const invite = document.querySelector('.classInvite')
+if (weekday == "Monday" || weekday == "Tuesday") {invite.style.display = 'block';}
+
+
 
