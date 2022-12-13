398f235f2ad5a75cfbe6713c937bcbeb038984e3
Straton11 <stratonfacer1@gmail.com>
2022-12-12 22:05:40 -0700

    about

--- bountiful-foods/scripts/main.js
+++ bountiful-foods/scripts/main.js
@@ -1,19 +1,3 @@
-const ctaEl = document.getElementById("cta");
-const bSmoothieEl = document.getElementById("blue-smoothie");
-const oSmoothieEl = document.getElementById("orange-smoothie");
-
-ctaEl.addEventListener('mouseenter', () =>{
-  bSmoothieEl.classList.add("bounce-blue");
-  setTimeout(()=>{
-    oSmoothieEl.classList.add("bounce-orange")
-  }, 500);
-});
-
-ctaEl.addEventListener('mouseleave', () =>{
-  bSmoothieEl.classList.remove("bounce-blue");
-  oSmoothieEl.classList.remove("bounce-orange");
-});
-
 // drink count
 let drinkCount = Number(window.localStorage.getItem("drinksMade"));
 document.getElementById('drink-total').textContent = drinkCount;
